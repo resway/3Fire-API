212396264b4eaff25c1efb1c0940e244977f1bc0
resway <jacky_gang@163.com>
2017-12-27 16:52:45 +0800

    change sql.sql

--- sql.sql
+++ sql.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS `user_group` (
   `update_time` int(11) DEFAULT NULL COMMENT '修改时间',
   `delete_time` int(11) DEFAULT NULL COMMENT '删除时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='用户分组表' AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='用户分组表' AUTO_INCREMENT=1 ;
 
 INSERT INTO `user_group` (`id`, `name`, `desc`, `create_time`, `update_time`, `delete_time`) VALUES
 (1, 'admin', '超级管理员', NULL, NULL, NULL),
@@ -29,6 +29,9 @@ CREATE TABLE IF NOT EXISTS `user` (
   `update_time` int(11) DEFAULT NULL COMMENT '最后登录时间',
   `delete_time` int(11) DEFAULT NULL COMMENT '删除时间',
   `update_ip` char(20) DEFAULT NULL COMMENT '最后登录ip',
+  UNIQUE KEY(`username`),
+  INDEX (`group_id`),
+  INDEX (`username`),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表' AUTO_INCREMENT=1 ;
 
